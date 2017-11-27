3ba78f9e209f811e032f0eda417a7f59ec5e4085
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-27 21:32:05 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -7,6 +7,7 @@ def dictionary
     "two" => '2',
     "too" => '2',
     "for" => '4',
+    "For" => '4'.
     "four" => '4',
     "be" => 'b',
     "you" => 'u',
@@ -35,5 +36,5 @@ end
 def bulk_tweet_shortener(tweets)
   tweets.each do |tweet|
     puts word_substituter(tweet)
-  end  
+  end
 end
