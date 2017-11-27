1011dc3e41e15b0cbf32abd0220b0baf2e65b73c
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-27 21:34:21 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,3 +38,9 @@ def bulk_tweet_shortener(tweets)
     puts word_substituter(tweet)
   end
 end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  end  
+each  
